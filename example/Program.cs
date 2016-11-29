2ff686eb0d772bb83fa31d6f0b53cddc5bf73d70
GucVit <gutsalyuk.vitaly@yandex.ru>
2016-11-29 08:12:22 +0300

    first commit

---
+++ example/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, sum;
+            Console.Clear();
+            a = 5;
+            b = 4;
+            sum = a + b;
+            Console.WriteLine();
+            Console.Write("Sum: " + sum);
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+    }
+}
